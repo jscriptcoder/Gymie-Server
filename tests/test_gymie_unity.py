c5febdadad576908ba2413350d2b825f8d837241
Francisco Ramos <jscriptcoder@gmail.com>
2020-10-13 12:19:36 +0200

    WIP: unity ml-agents envs

---
+++ tests/test_gymie_unity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import uuid
+import json
+import unittest
+import numpy as np
+import gymie.server as server
+import gymie.api as api
+from functools import reduce
+from test_base import TestBase
+from gymie.exceptions import *
+
+
+def unity_get_env(env_id, seed=None):
+    pass
+
+
+class TestGymieUnity(TestBase):
+    pass
