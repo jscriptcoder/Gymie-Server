ce9a8480c78d090d6320c7c3777f8af1b6e29882
Francisco Ramos <jscriptcoder@gmail.com>
2020-10-14 17:14:23 +0200

    Adding dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gymie",
-    version="0.0.1",
+    version="0.1.0",
     author="Francisco Ramos",
     author_email="francisco.ramos@researchlab.ai",
     description="WebSocket server that exposes an API to train AI agents on OpenAI Gym and gym-api-like Environments",
@@ -13,6 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jscriptcoder/Gymie-Server",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'eventlet==0.28.0',
+        'gym==0.17.3',
+        'uuid==1.30',
+    ],
+    extras_require={
+        'box2d': ['box2d-py==2.3.8'],
+        'retro': ['gym-retro==0.8.0'],
+        'unity': ['mlagents-envs==0.20.0', 'gym-unity==0.20.0'],
+    },
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
